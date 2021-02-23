3b79efba9b972be129e33b51eb4bce3974351d6b
Yevhenii Herasymenko <74263798+Zhekager@users.noreply.github.com>
2021-02-23 17:35:53 +0200

    Updates

--- closeMenu.4e65c2fe.js
+++ closeMenu.4e65c2fe.js
@@ -117,83 +117,9 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"closeMenu.js":[function(require,module,exports) {
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./..\\fonts\\dm-sans-v6-latin-regular.woff2":[["dm-sans-v6-latin-regular.a45f7eae.woff2","fonts/dm-sans-v6-latin-regular.woff2"],"fonts/dm-sans-v6-latin-regular.woff2"],"./..\\fonts\\dm-sans-v6-latin-regular.woff":[["dm-sans-v6-latin-regular.951d2140.woff","fonts/dm-sans-v6-latin-regular.woff"],"fonts/dm-sans-v6-latin-regular.woff"],"./..\\fonts\\dm-sans-v6-latin-500.woff2":[["dm-sans-v6-latin-500.01a97bb0.woff2","fonts/dm-sans-v6-latin-500.woff2"],"fonts/dm-sans-v6-latin-500.woff2"],"./..\\fonts\\dm-sans-v6-latin-500.woff":[["dm-sans-v6-latin-500.044febe0.woff","fonts/dm-sans-v6-latin-500.woff"],"fonts/dm-sans-v6-latin-500.woff"],"./..\\fonts\\dm-sans-v6-latin-700.woff2":[["dm-sans-v6-latin-700.d929dea6.woff2","fonts/dm-sans-v6-latin-700.woff2"],"fonts/dm-sans-v6-latin-700.woff2"],"./..\\fonts\\dm-sans-v6-latin-700.woff":[["dm-sans-v6-latin-700.42c96a41.woff","fonts/dm-sans-v6-latin-700.woff"],"fonts/dm-sans-v6-latin-700.woff"],"./..\\fonts\\titan-one-v8-latin-regular.woff2":[["titan-one-v8-latin-regular.c7773417.woff2","fonts/titan-one-v8-latin-regular.woff2"],"fonts/titan-one-v8-latin-regular.woff2"],"./..\\fonts\\titan-one-v8-latin-regular.woff":[["titan-one-v8-latin-regular.7f3e05d4.woff","fonts/titan-one-v8-latin-regular.woff"],"fonts/titan-one-v8-latin-regular.woff"],"./..\\images\\products\\mobile\\ice-cream-249.png":[["ice-cream-249.0a7811a8.png","images/products/mobile/ice-cream-249.png"],"images/products/mobile/ice-cream-249.png"],"./..\\images\\products\\tablet\\ice-cream-220.png":[["ice-cream-220.4fb2fc32.png","images/products/tablet/ice-cream-220.png"],"images/products/tablet/ice-cream-220.png"],"./..\\images\\products\\desktop\\ice-cream-335.png":[["ice-cream-335.bae3f3cc.png","images/products/desktop/ice-cream-335.png"],"images/products/desktop/ice-cream-335.png"],"./..\\images\\products\\mobile\\ice-coffee-249.png":[["ice-coffee-249.9da87a9d.png","images/products/mobile/ice-coffee-249.png"],"images/products/mobile/ice-coffee-249.png"],"./..\\images\\products\\tablet\\ice-coffee-220.png":[["ice-coffee-220.44eac9e7.png","images/products/tablet/ice-coffee-220.png"],"images/products/tablet/ice-coffee-220.png"],"./..\\images\\products\\desktop\\ice-coffee-337.png":[["ice-coffee-337.39eae97c.png","images/products/desktop/ice-coffee-337.png"],"images/products/desktop/ice-coffee-337.png"],"./..\\images\\products\\mobile\\milkshakes-249.png":[["milkshakes-249.606391d0.png","images/products/mobile/milkshakes-249.png"],"images/products/mobile/milkshakes-249.png"],"./..\\images\\products\\tablet\\milkshakes-220.png":[["milkshakes-220.af336313.png","images/products/tablet/milkshakes-220.png"],"images/products/tablet/milkshakes-220.png"],"./..\\images\\products\\desktop\\milkshakes-335.png":[["milkshakes-335.bc2400a7.png","images/products/desktop/milkshakes-335.png"],"images/products/desktop/milkshakes-335.png"],"./..\\images\\products\\dots.png":[["dots.5e0d1fe9.png","images/products/dots.png"],"images/products/dots.png"],"./..\\images\\about\\mobile\\sectionbg-1x-320x133-min.png":[["sectionbg-1x-320x133-min.8a003265.png","images/about/mobile/sectionbg-1x-320x133-min.png"],"images/about/mobile/sectionbg-1x-320x133-min.png"],"./..\\images\\about\\desktop\\sectionbg-1x-min.png":[["sectionbg-1x-min.0603fa21.png","images/about/desktop/sectionbg-1x-min.png"],"images/about/desktop/sectionbg-1x-min.png"],"./..\\images\\about\\mobile\\sectionbg-2x-320x133-min.png":[["sectionbg-2x-320x133-min.c93272b4.png","images/about/mobile/sectionbg-2x-320x133-min.png"],"images/about/mobile/sectionbg-2x-320x133-min.png"],"./..\\images\\about\\desktop\\sectionbg-2x-min.png":[["sectionbg-2x-min.34f7e7a4.png","images/about/desktop/sectionbg-2x-min.png"],"images/about/desktop/sectionbg-2x-min.png"],"./..\\images\\advantages\\icon1.png":[["icon1.7ef6014f.png","images/advantages/icon1.png"],"images/advantages/icon1.png"],"./..\\images\\advantages\\icon2.png":[["icon2.f7a30458.png","images/advantages/icon2.png"],"images/advantages/icon2.png"],"./..\\images\\advantages\\icon3.png":[["icon3.42b26187.png","images/advantages/icon3.png"],"images/advantages/icon3.png"],"./..\\images\\customer-reviews\\lap.svg":[["lap.605e7151.svg","images/customer-reviews/lap.svg"],"images/customer-reviews/lap.svg"],"./..\\images\\customer-reviews\\ellipse.svg":[["ellipse.d23e561b.svg","images/customer-reviews/ellipse.svg"],"images/customer-reviews/ellipse.svg"],"./..\\images\\customer-reviews\\home.svg":[["home.69d575fa.svg","images/customer-reviews/home.svg"],"images/customer-reviews/home.svg"],"./..\\images\\customer-reviews\\ellipse9.svg":[["ellipse9.00328401.svg","images/customer-reviews/ellipse9.svg"],"images/customer-reviews/ellipse9.svg"],"./..\\images\\contacts\\contacts-bg.png":[["contacts-bg.85dfb50b.png","images/contacts/contacts-bg.png"],"images/contacts/contacts-bg.png"],"./..\\images\\contacts\\contacts-bg@2x.png":[["contacts-bg@2x.9c87d2e1.png","images/contacts/contacts-bg@2x.png"],"images/contacts/contacts-bg@2x.png"],"./..\\images\\contacts\\contacts-bg@3x.png":[["contacts-bg@3x.06e1a39f.png","images/contacts/contacts-bg@3x.png"],"images/contacts/contacts-bg@3x.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./sass/main.scss");
-},{"./sass/main.scss":"sass/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -397,5 +323,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","closeMenu.js"], null)
+//# sourceMappingURL=/closeMenu.4e65c2fe.js.map
